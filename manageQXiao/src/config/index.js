3351c7c3ebb2053118322b2023c619f9d8ab8c7e
huangdesheng <qq1083879945@126.com>
2019-12-02 11:18:36 +0800

    hds:提交项目

--- manageQXiao/src/config/index.js
+++
@@ -1,2 +0,0 @@
-const IS_DEV = process.env.NODE_ENV !== 'production';
-export const API_ROOT = IS_DEV ? 'http://192.168.18.107:8080/qxiao-cms/' : 'http://ring.qxiao.net/qxiao-cms/';
--- manageQXiao/src/utils/lazyLoading.js
+++
@@ -1,2 +0,0 @@
-export default (name) => () =>
-import(`@/views/${name}`)
