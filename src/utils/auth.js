fd5a39d282dccd46d6d7b893a25013623705d405
huangdesheng <qq1083879945@126.com>
2020-09-30 11:05:38 +0800

    20200930代码封包

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,16 @@
-import Cookies from 'js-cookie';
+import Cookies from 'js-cookie'
 
 //获取Authorization
 export function getToken() {
-  return Cookies.get('Authorization');
+  return Cookies.get('Authorization')
 }
 
 //设置Authorization
 export function setToken(token) {
-  return Cookies.set('Authorization', token);
+  return Cookies.set('Authorization', token)
 }
 
 //移除Authorization
 export function removeToken() {
-  return Cookies.remove('Authorization');
+  return Cookies.remove('Authorization')
 }
--- src/views/device/configuration/index.vue
+++ src/views/device/configuration/index.vue
@@ -262,7 +262,6 @@ export default {
         apMac: row.apMac,
         schoolId: row.schoolId
       };
-
       this.deviceNo = row.deviceNo;
       this.deviceMac = row.apMac;
       this.queryApInfo(data);
